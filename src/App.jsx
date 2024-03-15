4734b694142ff1f760ce769c341146c6ef9f1e0f
anissarkeraraf <anissarkeraraf@gmail.com>
2024-03-15 10:59:50 +0600

    added the shopping cart

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="main-container">
+        <div className="cards-container">
+          <h1>This is card</h1>
+        </div>
+        <div className="cart-container">
+          <h1>This is cart</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
